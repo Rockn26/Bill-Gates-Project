caf5b0e5d3633eea1eb393053a016d64716a03f9
Rockn26 <irfanank26@gmail.com>
2024-02-10 15:19:24 +0300

    create profile component

---
+++ src/Component/Profile/Profile.jsx
@@ -0,0 +1,16 @@
+
+import "./Profile.style.css";
+
+function Profile() {
+
+  return (
+    <>
+    <div className='container'>
+        <img src="src/assets/billgates.jpg" alt="" />
+        <h2>Spend Bill Gates' Money</h2>
+    </div>
+    </>
+  )
+}
+
+export default Profile

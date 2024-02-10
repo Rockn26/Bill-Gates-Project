0ea3cc6ba224d474bd5d1dcbaf7033eed4c87a63
Rockn26 <irfanank26@gmail.com>
2024-02-10 15:39:34 +0300

    add profile and moneybar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import MoneyBar from './Component/MoneyBar/MoneyBar'
+import Profile from './Component/Profile/Profile.jsx';
 
 function App() {
 
   return (
     <>
-      
+      <Profile />
+      <MoneyBar />
+
     </>
   )
 }

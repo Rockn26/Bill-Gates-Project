6b3735c03a8ac635f42122e41dafe8e4869cdbe1
Rockn26 <irfanank26@gmail.com>
2024-02-10 15:37:36 +0300

    add money-bar component

---
+++ src/Component/MoneyBar/MoneyBar.jsx
@@ -0,0 +1,15 @@
+import "./MoneyBar.stlye.css"
+
+function MoneyBar() {
+
+    return (
+      <>
+      <div className="money-bar-container">
+        <h2>$<span>100000000000</span></h2>
+      </div>
+      </>
+    )
+  }
+  
+  export default MoneyBar
+  

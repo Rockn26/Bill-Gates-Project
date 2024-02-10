4a3516dffd5268f50804253de0c8b1098666d4b6
Rockn26 <irfanank26@gmail.com>
2024-02-10 15:46:54 +0300

    create ItemContrainer component

---
+++ src/Component/ItemContainer/ItemContainer.jsx
@@ -0,0 +1,14 @@
+import "./ItemContainer.style.css";
+
+function ItemContainer() {
+
+    return (
+      <>
+      <div className="item-main-container">
+        
+      </div>
+      </>
+    )
+  }
+  
+  export default ItemContainer
